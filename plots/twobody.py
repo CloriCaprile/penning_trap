dc6fc7b72e8e4e9b3751fdf4a09f8186ee594390
Luca Nigro <luca.nigro@studenti.unimi.it>
2022-10-15 15:42:42 +0200

    Add plots/ folder and python scripts

---
+++ plots/twobody.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df_int = pd.read_csv("data2part_int_64000.txt", names=["p", "t", "x", "y", "z", "vx", "vy", "vz"], sep=" ")
+df_nonint = pd.read_csv("data2part_nonint_64000.txt", names=["p", "t", "x", "y", "z", "vx", "vy", "vz"], sep=" ")
+
+# Plot style options
+w = 10
+h = 5
+fontsize = 12
+save_fig = True
+
+
